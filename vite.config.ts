b4192654c9445674107119352dab5c424f5f2878
prisha777 <88456710+prisha777@users.noreply.github.com>
2025-12-26 08:03:53 +0000

    Deploy via GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+  base: "/prisha-goel.com/",
   resolve: {
     alias: {
       // Alias @ to the src directory
@@ -17,3 +17,4 @@ export default defineConfig({
     },
   },
 })
+
